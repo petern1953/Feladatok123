716de46dc43c8caf8b6f4bd54dba9d0759bd65c3
petern1953 <petern1953@gmail.com>
2020-11-15 16:49:17 +0100

    3. feladat ok

---
+++ js/feladat03.js
@@ -0,0 +1,18 @@
+// 3. Feladat
+// Hozz létre egy függvénykifejezést arrow function-nel!
+// A függvény neve 'personDataLog' legyen.
+// A függvény paraméterként egy objektumot vár, ami egy
+// 'firstname', 'lastname', és 'age' tulajdonságokkal rendelkezik.
+// Amennyiben bármelyik tulajdonság hiányozna, úgy rendre a következő
+// alapértelmezett értékeket vegyék fel:
+// - firstname: John, - lastname: Doe, - age: 33
+// A függvény kiírja az adott illető adatait a konzolra az adott formátumban:
+// My name is 'firstname', 'lastname'. I'm 'age' years old.
+// A firstname, lastname és age helyére a paraméterként kapott
+// objektumtulajdonságok értékét kell behelyettesíteni.
+
+
+const personDataLog = ({ firstName = 'John', lastName = 'Doe', age = 33 } = {}) =>
+    console.log("My name is " + firstName + " " + lastName + ". I'm " + age + " years old.");
+
+console.log(personDataLog({ firstName: 'Kuku', lastName: 'Benkó' }));
