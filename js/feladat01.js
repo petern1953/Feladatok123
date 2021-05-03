6f6b0b2e6f2edbbd0a1f1f1a5afae93e89e55044
petern1953 <petern1953@gmail.com>
2021-05-03 08:22:56 +0200

    1

--- js/feladat01.js
+++ js/feladat01.js
@@ -3,11 +3,11 @@
 // A gombok szövegei az alábbiak legyenek: 
 // Első, Második, Harmadik. Mindegyik gombnak add meg a 'button' class-t!
 // Készíts egy JavaScript fájlt, és linkeld be a html oldaladba! 
-// Írj egy függvény kifejezést arrow function segítségével.
+// Írj egy függvénykifejezést arrow function segítségével.
 // A függvény neve handleClick legyen. 
 // A függvény a meghívása után mindegyik gombhoz hozzáad egy eseményfigyelőt, amely
 // kattintásra kiírja a gomb szövegét a konzolra. A függvényen belül ciklust használj, 
-// tehát ne manuálisan egyesével add a gombokhoz az eseményfigyelőt!
+// tehát ne manuálisan, egyesével add a gombokhoz az eseményfigyelőt!
 
 'use strict'
 
